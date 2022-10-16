22ee53bfe7a9f50cebac09f0ac7ee865d1b12f91
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-10-15 20:24:30 -0400

    commit tree ebd627f3a0a6a78a05bc94062ae994e7672440db

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (dwarves)
+  dwarves.each.with_index(1) {|dwarve, index| puts "#{index}. #{dwarve}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (planeteer_calls)
+  planeteer_calls.map {|planeteer| "#{planeteer.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (assorted_words)
+  if assorted_words.include? (assorted_words.find {|word| word.length > 4})
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.find {|ingredient| cheese_types.include?(ingredient)}
 end
